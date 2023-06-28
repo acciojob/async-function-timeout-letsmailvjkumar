c2233c09c69b3b27de89e07a046bd95b94b2cf29
VJKUMAR <115359380+letsmailvjkumar@users.noreply.github.com>
2023-06-28 13:07:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,8 +6,6 @@ document.getElementById('btn').addEventListener('click', async () => {
   const message = textInput.value;
   const delay = Number(delayInput.value) * 1000; // Convert delay to milliseconds
 
-  outputDiv.textContent = 'Loading...';
-
   await new Promise(resolve => setTimeout(resolve, delay));
 
   outputDiv.textContent = message;
